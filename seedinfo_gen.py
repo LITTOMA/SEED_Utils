16df6a72420015b9ec8ab837e6d752a02a85aefc
LITTOMA <littoma@163.com>
2015-07-17 14:16:37 +0800

    Some adjustment.

--- seedinfo_gen.py
+++ seedinfo_gen.py
@@ -11,15 +11,20 @@ def GetIDandSEED(filename):
             tidoffset = (struct.unpack('<I',savedata[SBoffset+24:SBoffset+28])[0] - 1) * 4096 + SBoffset - 52 + 4096
             seedoffset = tidoffset + 16000
             titlecount = struct.unpack('<I',savedata[tidoffset-4092:tidoffset-4088])[0]
-            for i in range(titlecount):
-                tid = savedata[tidoffset:tidoffset+8]
-                seed = savedata[seedoffset:seedoffset+16]
-                if not tid in tids:
-                    tids.append(tid)
-                    seeds.append(seed)
-                tidoffset += 8
-                seedoffset += 16
-            savedata = savedata[seedoffset:]
+            try:
+                for i in range(titlecount):
+                    tid = savedata[tidoffset:tidoffset+8]
+                    seed = savedata[seedoffset:seedoffset+16]
+                    if not tid in tids:
+                        tids.append(tid)
+                        seeds.append(seed)
+                        print 'TitleID: ' + hexlify(reverseByteArray(tid)),' SEED: ' + hexlify(seed)
+                    tidoffset += 8
+                    seedoffset += 16
+            except:
+                print 'No SEED found in this partition.'
+            finally:
+                savedata = savedata[seedoffset:]
     return tids,seeds
 
 def reverseByteArray(bytearr):
@@ -34,18 +39,27 @@ if __name__ == '__main__':
     if len(sys.argv)<2:
         print 'Usage: seedinfo_gen.py file(s)'
         sys.exit()
+    
+    if '-h'in sys.argv or '-help' in sys.argv:
+        print 'Usage: seedinfo_gen.py file(s)\n'
+        print "SEEDDB is stored in 'nand:\\data<console-unique>\\sysdata\\0001000f\\'"
+        print 'Decyrpt NAND FAT16 partition and dump these files for this script.'
+        sys.exit()
+    
     for filename in sys.argv[1:]:
-        tids += GetIDandSEED(filename)[0]
-        seeds += GetIDandSEED(filename)[1]
+        titleid,seed = GetIDandSEED(filename)
+        tids += titleid
+        seeds += seed
+    
+    if len(tids)<1:
+        print 'No SEED found.'
+        sys.exit()
 
     outpath = os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), 'seedinfo.bin')
     with open(outpath,'wb') as seedinfo:
         seedinfo.write('SEED')
         seedinfo.write(struct.pack('<I',len(tids)))
-        i = 0
         for tid in tids:
             seedinfo.write(tid)
-            print 'TitleID: ' + hexlify(reverseByteArray(tid)),'SEED: ' + hexlify(seeds[i])
-            i += 1
         for seed in seeds:
             seedinfo.write(seed)
